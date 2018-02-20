4d159d29eeddc5573fb5944fcea46bcc1fcc07db
secretID2 <secretidentity009@gmail.com>
2018-02-20 16:36:09 +0000

    Node js chat that knows client identity DONEE!!!

--- Websocket/chat-example-master/index.js
+++ Websocket/chat-example-master/index.js
@@ -2,14 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var clients=[];
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    console.log("Connected to websocket:"+socket.id); 
+    clients.push(socket.id);
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+     
+    //io.emit('chat message', msg);
+    
+        
+    io.sockets.connected[clients[0]].emit("chat message", "Howdy, User 1!");
+    io.sockets.connected[clients[1]].emit("chat message", "Hey there, User 2");
+        
+     
+    
   });
 });
 
